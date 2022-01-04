6dfb9dd31efe018f9214da93e92590b07b8c15ec
matias Seisdedos <matiasseisdedos@matiass-MBP.attlocal.net>
2022-01-04 14:03:24 -0500

    Portfolio done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,31 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:786-627-9797'>786-627-9797</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Gmail</LinkTitle>
+          <LinkItem href='mailto:matiasseisdedosg@gmail.com'>matiasseisdedosg@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href='https://wwww.github.com/matiseisdedos'>
+            <AiFillGithub size='3rem' />
+          </SocialIcons>
+          <SocialIcons href='https://wwww.linkedin.com/in/matiasseisdedosgiletta'>
+            <AiFillLinkedin size='3rem' />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
